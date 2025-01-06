95f71081ba841eb7184cb5a2e32e951af8c58d92
Shahryar <dev.shahryar@gmail.com>
2025-01-06 22:03:26 +0330

    fix(internal/project): change and fix some types and structs

--- internal/project/domain/project.go
+++ internal/project/domain/project.go
@@ -2,20 +2,26 @@ package domain
 
 import (
 	"errors"
+	"fmt"
+	"time"
 
 	"github.com/google/uuid"
 )
 
-const (
-	ERROR_ID_NIL = "ID cannot be a Nil UUID"
+var (
+	ERROR_ID_NIL = errors.New("ID cannot be a Nil UUID")
+	ERROR_ID_VALID = errors.New("ID is not valid")
+	ERROR_PARENT = errors.New("Cannot be self Parent")
 )
 
 type ProjectID = uuid.UUID
 
 type Project struct {
-	UUID   ProjectID
-	Name   string
-	Parent *Project
+	UUID     ProjectID
+	Name     string
+	Parent   ProjectID
+	CreateAt time.Time
+	DeleteAt time.Time
 }
 
 type ProjectFilter struct {
@@ -24,7 +30,13 @@ type ProjectFilter struct {
 
 func (p *Project) IsValid() error {
 	if p.UUID == uuid.Nil {
-		return errors.New(ERROR_ID_NIL)
+		return ERROR_ID_NIL
 	}
-	return uuid.Validate(p.UUID.String())
+	if err:= uuid.Validate(p.UUID.String());err!=nil{
+		return fmt.Errorf("%w : %w" , ERROR_ID_VALID , err)
+	}
+	if p.UUID == p.Parent {
+		return ERROR_PARENT
+	}
+	return nil
 }
--- internal/project/port/project.go
+++ internal/project/port/project.go
@@ -8,7 +8,7 @@ import (
 type Repo interface{
 	Create(ctx context.Context, record domain.Project)(domain.ProjectID , error)
 	Update(ctx context.Context, UUID domain.ProjectID , newRecord domain.Project) error
-	GetAllProjects(ctx context.Context , pageIndex , pageSize)([]domain.Project , error)
+	GetAllProjects(ctx context.Context , pageIndex , pageSize uint)([]domain.Project , error)
 	FilterProject(ctx context.Context , pageIndex , pageSize uint , filter domain.ProjectFilter)([]domain.Project , error)
 	Delete(ctx context.Context , UUID domain.ProjectID) error
 }

622b56bdd2a97b264d2d38e162cce768397f3dce
Shahryar <dev.shahryar@gmail.com>
2025-01-06 14:59:36 +0330

    feat(internal/project): fix project domain service

--- internal/project/service.go
+++ internal/project/service.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	projectDomain "donezo/internal/project/domain"
 	"donezo/internal/project/port"
+	"fmt"
+
+	"github.com/google/uuid"
 )
 
 type service struct {
@@ -17,16 +20,44 @@ func NewService(repo port.Repo) port.Service {
 }
 
 func (s *service) CreateProject(ctx context.Context, record projectDomain.Project) (projectDomain.ProjectID, error) {
-	panic("")
-}
-func (s *service) UpdateProject(ctx context.Context, uuid projectDomain.ProjectID, newRecord projectDomain.Project) error {
-	panic("")
-}
-
-func (s *service) GetProject(ctx context.Context, pageIndex uint, pageSize uint, filters ...projectDomain.ProjectFilter) ([]projectDomain.Project, error) {
-	panic("")
+	if err:= record.IsValid() ; err!=nil{
+		return uuid.Nil , fmt.Errorf("%w" , err)
+	}
+	projectID , err := s.repo.Create(ctx,record)
+	return projectID , err
 }
 
-func (s *service) DeleteProject(ctx context.Context, uuid projectDomain.ProjectID) error {
-	panic("")
+func (s *service) UpdateProject(ctx context.Context, UUID projectDomain.ProjectID, newRecord projectDomain.Project) error {
+	if err:= uuid.Validate(UUID.String()); err!=nil{
+		return fmt.Errorf("%w" , err)
+	}
+	if err := s.repo.Update(ctx , UUID , newRecord); err!=nil {
+		return fmt.Errorf("%w" , err)
+	}
+	return nil
+}
+
+func (s *service) GetProject(ctx context.Context, pageIndex uint, pageSize uint, filter *projectDomain.ProjectFilter) ([]projectDomain.Project, error) {
+	var projects []projectDomain.Project
+	var err error
+	if filter != nil{
+		projects , err = s.repo.FilterProject(ctx , pageIndex , pageSize , *filter)
+	}else {
+		projects , err = s.repo.GetAllProjects(ctx,pageIndex , pageSize)
+	}
+	if err!=nil{ 
+		return projects , fmt.Errorf("%w" , err)
+	}
+	return projects , err 
+}
+
+func (s *service) DeleteProject(ctx context.Context, UUID projectDomain.ProjectID) error {
+ if err := uuid.Validate(UUID.String()); err!=nil{
+	 return fmt.Errorf("%w" , err)
+ }
+ if err := s.repo.Delete(ctx , UUID); err!=nil{
+ 
+	 return fmt.Errorf("%w" , err)
+ }
+ return nil
 }

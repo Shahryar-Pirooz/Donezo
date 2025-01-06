833631ff05d1cd8df674512836555974d3dc495a
Shahryar <dev.shahryar@gmail.com>
2025-01-06 14:58:45 +0330

    fix(internal/porject/port): fix ports

--- internal/project/port/service.go
+++ internal/project/port/service.go
@@ -9,6 +9,6 @@ import (
 type Service interface {
 	CreateProject(ctx context.Context , record projectDomain.Project) (projectDomain.ProjectID , error)
 	UpdateProject(ctx context.Context , uuid projectDomain.ProjectID , newRecord projectDomain.Project) error
-	GetProject(ctx context.Context , pageIndex uint , pageSize uint , filters ...projectDomain.ProjectFilter) ([]projectDomain.Project , error)
+	GetProject(ctx context.Context , pageIndex uint , pageSize uint , filter *projectDomain.ProjectFilter) ([]projectDomain.Project , error)
 	DeleteProject(ctx context.Context , uuid projectDomain.ProjectID) error
 }
